7ea04080e1320468a92fe84eccfbd92eea26560c
stephcmartin <martin.stephaniechow@gmail.com>
2018-02-05 21:53:42 -0500

    JSON data from api now renders on UI
    - Appended text to show data from UI
    - Temporary hide search bar function as it no longer serves purpose
    - Have not added 'yes' or 'no' to the question yet

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-let latitude = '';
-let longitude = '';
-let tempC = '';
+let latitude = ''
+let longitude = ''
+// setting time
+const time = new Date().getHours()
+console.log('time is', time)
 
+if( time >= 6 &&  time <= 12){
+  let d = document.querySelector('.day')
+  d.innerText = 'Morning'
+  }
+else if( time > 12 &&  time <= 18){
+  let d = document.querySelector('.day')
+  d.innerText = 'Day'
+  }
+ else if( time > 18 &&  time < 22){
+  let d = document.querySelector('.day')
+  d.innerText = 'Evening'
+  }
+else{
+   let d = document.querySelector('.day')
+   d.innerText = 'Night'
+ }
+
+// retriving location
 $(document).ready(function() {
   $.getJSON('http://ipinfo.io', function(data) {
-    console.log('ipin data is', data)
+    console.log('ipin data is', data),
+    console.log('data.loc is', data.loc),
+    console.log(data.region, data.country),
+    $(".location").text(data.region),
+    $('.location').append(' , ', data.country),
     let location = data.loc.split(',');
     latitude = location[0];
     longitude = location[1];
@@ -12,6 +36,7 @@ $(document).ready(function() {
   })
 })
 
+// updating UI
 function getWeather(latitude, longitude) {
   let url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=d69f2008e97dcdb3702715154c478061';
   console.log('url for location is', url)
@@ -21,11 +46,18 @@ function getWeather(latitude, longitude) {
     dataType: 'jsonp',
     success: function (data){
       console.log('data is', data),
-      console.log(data.main.temp),
-      console.log('the incorrect temperture is', (data.main.temp - 273.15))
+      console.log('the incorrect temperture in C is', (data.main.temp - 273.15)),
+      $(".temp").innerHTML(Math.round(data.main.temp - 273.15),'°C'),
+      // $('.temp').append('°C'),
+      console.log('moisture', data.weather[0].main),
+      $('.moisture').text(data.weather[0].main),
+      console.log('wind data', data.wind.speed),
+      $('.w').text(data.wind.speed),
+      $('.h').text(data.main.humidity)
     }
   })
 }
+
 
 $("#submitWeather").click(function () {
   event.preventDefault()
